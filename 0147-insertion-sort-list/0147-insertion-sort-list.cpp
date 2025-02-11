62bc647a63ebfa1b99ac56cd9f7ed8c8d48bae45
Pratyush jain <101690733+ultimateebeast@users.noreply.github.com>
2025-02-11 23:19:12 +0530

    Time: 0 ms (100%), Space: 15.1 MB (8%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.cpp
+++ 0147-insertion-sort-list/0147-insertion-sort-list.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
     ListNode* insertionSortList(ListNode* head) {
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = head;
+        // ListNode* dummy = new ListNode(0);
+        // ListNode* curr = head;
 
-        while(curr!=nullptr){
-            ListNode* prev = dummy;
-            ListNode* nextNode = curr->next;
-            while(prev->next!=nullptr && prev->next->val < curr->val){
-                prev = prev -> next;
-            }
-            curr->next = prev->next;
-            prev->next = curr;
-            curr = nextNode; 
+        // while(curr!=nullptr){
+        //     ListNode* prev = dummy;
+        //     ListNode* nextNode = curr->next;
+        //     while(prev->next!=nullptr && prev->next->val < curr->val){
+        //         prev = prev -> next;
+        //     }
+        //     curr->next = prev->next;
+        //     prev->next = curr;
+        //     curr = nextNode; 
+        // }
+        // return dummy->next;
+
+        vector<ListNode*>res;
+
+        while(head){
+            res.push_back(head);
+            head = head->next;
         }
-        return dummy->next;
+
+        sort(res.begin(),res.end(),[&](ListNode* a, ListNode* b){
+            return a->val > b->val;
+        });
+
+        ListNode* prev = nullptr;
+
+        for(int i=0;i<res.size();i++){
+            head = res[i];
+            head->next = prev;
+            prev = head;
+        }
+
+        return head;
     }
 };
