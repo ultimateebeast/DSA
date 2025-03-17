9b8eed9f60ca482bf34dce935acec435e066ffbc
Pratyush jain <101690733+ultimateebeast@users.noreply.github.com>
2025-03-17 10:17:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+class Solution {
+  public:
+    int nthFibonacci(int n) {
+        // code here
+        if(n <= 1) return n;
+        
+        int prev = nthFibonacci(n - 1);
+        int prev1 = nthFibonacci(n - 2);
+        
+        return prev + prev1;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.nthFibonacci(n) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
