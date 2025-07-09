d1834f34162f6c1885b0905ab65913c956e8a35e
Pratyush <101690733+ultimateebeast@users.noreply.github.com>
2025-07-10 01:25:17 +0530

    Time: 3 ms (29.17%), Space: 10.7 MB (39.61%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> temp;
-        do {
-                temp.push_back(nums);
+        vector<vector<int>> res;
+        vector<int> pth;
+        vector<bool> check(nums.size(), false);
 
-        } while (next_permutation(nums.begin(), nums.end()));
+        backtrack(nums, check, res, pth);
+        
+        return res;
+    }
+private: 
+    void backtrack(vector<int> &nums, vector<bool> &check, vector<vector<int>> &res, vector<int> &pth){
+        if(pth.size() == nums.size()){
+            res.push_back(pth);
+        }
 
-        return temp;
+        for(int i = 0; i < nums.size(); i++){
+            if(check[i]) continue;
+
+            pth.push_back(nums[i]);
+            check[i] = true;
+
+            backtrack(nums, check, res, pth);
+
+            pth.pop_back();
+            check[i] = false;
+        }
     }
 };
