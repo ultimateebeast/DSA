8abfe7b5dae7fd7c8263bff1ad318d20edf6d0f4
Pratyush <101690733+ultimateebeast@users.noreply.github.com>
2025-07-16 12:17:59 +0530

    Time: 12 ms (13.02%), Space: 23.9 MB (12.17%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        if(nums.size() == 1) return nums[0];
+        unordered_map<int, int> cnt;
+
+        for(int val : nums){
+            cnt[val]++;
+        }
+
+        for(auto val : cnt){
+            if(val.second == 1) return val.first;
+        }
+
+        return -1;
+    }
+};
